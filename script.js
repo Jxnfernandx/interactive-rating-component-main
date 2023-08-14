7f448c3b6c5eacd6bb0feeaa8c66abe8db2b51b2
Jxnfernandx <orangutangklan@gmail.com>
2023-08-14 18:38:57 -0500

    Final Commit

--- script.js
+++ script.js
@@ -4,11 +4,39 @@ const submitBtn = document.querySelector("input")
 const rateBtn = document.getElementsByClassName(".btn")
 
 
-submitBtn.addEventListener("click", () => {
-        thankYou.classList.remove("hidden");
-        mainContainer.style.display = "none";
-    })
+const btns = document.querySelectorAll("button");
+let selectedButton = null;
 
+for (let i = 0; i < btns.length; i++) {
+    const button = btns[i];
     
+    button.addEventListener("click", () => {
+        if (selectedButton) {
+            selectedButton.style.backgroundColor = "";
+            selectedButton.style.color = "";
+        }
+        
+        selectedButton = button;
+        selectedButton.style.backgroundColor = "hsl(217, 12%, 63%)";
+        selectedButton.style.color = "white";
+        
+        const userRating = button.value;
+        document.getElementById("user-rating").textContent = userRating;
+    });
+}
 
+document.addEventListener("click", (event) => {
+    if (!event.target.closest(".main-container")) {
+        if (selectedButton) {
+            selectedButton.style.backgroundColor = "";
+            selectedButton.style.color = "";
+            selectedButton = null;
+            document.getElementById("user-rating").textContent = "0";
+        }
+    }
+});
 
+submitBtn.addEventListener("click", () => {
+    thankYou.classList.remove("hidden");
+    mainContainer.style.display = "none";
+})
